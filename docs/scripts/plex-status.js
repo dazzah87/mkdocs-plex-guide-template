a7dc17f19b4484d411a83070e1de12e6c5cfdee4
dazzah87 <7974733+dazzah87@users.noreply.github.com>
2026-08-04 12:26:43 +0200

    Update plex-status.js

--- docs/scripts/plex-status.js
+++ docs/scripts/plex-status.js
@@ -11,19 +11,18 @@ document.addEventListener("DOMContentLoaded", function() {
             let color = "grey";
             let germanDescription = data.status.description; // Fallback auf das Original, falls was Unbekanntes kommt
 
-            // Farben und deutsche Übersetzungen zuweisen je nach Status
             if (indicator === "none") {
                 color = "#2fcc66"; // Grün
-                germanDescription = "Alle Systeme arbeiten normal";
+                germanDescription = "Alle Systeme sind verfügbar. Keine Störungen oder Einschränkungen bekannt.";
             } else if (indicator === "minor") {
                 color = "#f1c40f"; // Gelb
                 germanDescription = "Leichte Einschränkungen";
             } else if (indicator === "major") {
                 color = "#e67e22"; // Orange
-                germanDescription = "Schwere Störung";
+                germanDescription = "Schwere Störung!";
             } else if (indicator === "critical") {
                 color = "#e74c3c"; // Rot
-                germanDescription = "Kritischer Ausfall";
+                germanDescription = "Kritischer Ausfall!";
             } else if (indicator === "maintenance") {
                 color = "#3498db"; // Blau
                 germanDescription = "Wartungsarbeiten";
